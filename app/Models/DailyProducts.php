8e7faa6853b628abe2a6fc2b3771fdd62b7e9137
viktorpromofarma <viktor.santos@promofarma.com.br>
2026-05-14 14:32:32 -0300

    feat: alterado o model para uma view já filtrado

--- app/Models/DailyProducts.php
+++ app/Models/DailyProducts.php
@@ -10,7 +10,7 @@ class DailyProducts extends Model
 {
     protected $connection  = 'sqlsrv';
 
-    protected $table = 'ETIQUETA_PLACAS_RESULTADO';
+    protected $table = 'VW_ETIQUETAS_PLACAS_RESULTADOS';
 
     protected $primaryKey = 'ID';
 
@@ -19,26 +19,8 @@ class DailyProducts extends Model
 
     public static function getDailyProducts($loja)
 {
-    $ids = Logs::all()
-        ->flatMap(function ($log) {
-            preg_match('/[\["](?:IDS:|IDS"\s*:\s*")[^\d]*([\d,\s]+)/', $log->COMANDO_EXECUTADO, $m);
-            return isset($m[1]) ? array_map('trim', explode(',', $m[1])) : [];
-        })
-        ->filter()
-        ->unique()
-        ->values()
-        ->toArray();
-
-    $query = DailyProducts::query()
-        ->whereNotNull('ID_TEMPLATE')
-        ->whereNotNull('LOJA')
-        ->where('loja', $loja);
-
-    foreach (array_chunk($ids, 2000) as $chunk) {
-        $query->whereNotIn('ID', $chunk);
+ 
+        return self::where('LOJA', $loja)
+            ->get();
     }
-
-    return $query->get();
-}
-
 }
